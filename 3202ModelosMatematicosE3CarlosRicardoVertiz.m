352ec902ee69922b8018ac364188a3926867eef0
CarlosRicardoVertiz <82452690+CarlosRicardoVertiz@users.noreply.github.com>
2021-04-15 22:40:09 -0500

    Add files via upload

---
+++ 3202ModelosMatematicosE3CarlosRicardoVertiz.m
@@ -0,0 +1,22 @@
+% Titulo: modelos Matematicos
+% Descripcion: Scrip para graficar: 
+%%3) f3 (x) = 5?2x, ¿-4 pertenece Rf2?, ¿-1/2 pertenece D?
+% Autor: Carlos Ricardo Vertiz
+% Fecha: Miercoles 14 de abril del 2021
+
+
+	% limpiar variables
+		 clear
+	% Ejemplo f3 (x) = 5?2x
+	% Rango de -5..5 en i = 1
+		x = -5: 0.1 : 5 ;
+	% Valor de la función
+	 	y=nthroot(2*(x),5);
+	% Dibujar x, y
+		 plot (x, y);
+	% Titulo
+		title ( "y=v(5&2x)" );
+	% Etiqueta para x
+		xlabel ( " x " );
+	% Etiqueta para y
+		ylabel ( " f (x) " );
