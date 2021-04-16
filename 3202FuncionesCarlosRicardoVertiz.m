1929fef42d6ac589ea33753e24befda920a584ec
CarlosRicardoVertiz <82452690+CarlosRicardoVertiz@users.noreply.github.com>
2021-04-16 03:28:41 -0500

    Add files via upload

--- 3202FuncionesCarlosRicardoVertiz.m
+++ 3202FuncionesCarlosRicardoVertiz.m
@@ -1,24 +1,20 @@
-% Titulo: modelos Matematicos
-% Descripcion: Scrip para graficar: 
-%1) f1(x) = x² +3 si  pertenece (-5, infinito)
-
+%Titulo : Funciones 
+% Descripción: Script para graficar la funcion: 
+%"x = 16 -x^2 / 4-x"
 % Autor: Carlos Ricardo Vertiz
-% Fecha: Jueves 15 de abril del 2021
+% Fecha: 15/04/2021
 
-% f1(x) = x² +3 
-%limpiar variables
+% limpiar variables
 clear
-%rango de -4 ..5 en i = 1
-
-x=-4:1:4;
-
-%valor de la funcion
-f1=-x.^2+3; 
-%dibujar x,y 
-plot (x,f1)
-%titulo
-title("f1(x)=-x^2+3; ");
-%etiqueta para x
-xlabel ("x");
-%etiqueta para y
-ylabel ("f1(x)");
+%rango de -5..5 en i=1
+x = -2 : 4 : 5 ;
+% Valor de la función
+f = (16-x.^2)/(4)-x;
+% Dibujar x, y
+plot(f,-x);
+% Titulo
+title ( "x = 16 -x^2 / 4-x" );
+% Etiqueta para x
+xlabel ( " x " );
+% Etiqueta para y
+ylabel ( " f(x) " );
