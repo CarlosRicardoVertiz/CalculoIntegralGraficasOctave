5c9072b340776b6b9293afe3c539f61209a7247a
CarlosRicardoVertiz <82452690+CarlosRicardoVertiz@users.noreply.github.com>
2021-04-15 19:50:26 -0500

    Add files via upload

---
+++ 3202ModelosMatematicosCarlosRicardoVertiz.m
@@ -0,0 +1,24 @@
+% Titulo: modelos Matematicos
+% Descripcion: Scrip para graficar: 
+%1) f1(x) = x² +3 si  pertenece (-5, infinito)
+
+% Autor: Carlos Ricardo Vertiz
+% Fecha: Jueves 15 de abril del 2021
+
+% f1(x) = x² +3 
+%limpiar variables
+clear
+%rango de -4 ..5 en i = 1
+
+x=-4:1:4;
+
+%valor de la funcion
+f1=-x.^2+3; 
+%dibujar x,y 
+plot (x,f1)
+%titulo
+title("f1(x)=-x^2+3; ");
+%etiqueta para x
+xlabel ("x");
+%etiqueta para y
+ylabel ("f1(x)");
